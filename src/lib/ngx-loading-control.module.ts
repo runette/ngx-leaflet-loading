ab6e2a10fb87c05e9a67f3e7c5a03c489fc7774a
runette <runette@gmail.com>
2019-12-14 18:29:21 +0200

    Revert "Revert "v0.0.1""
    This reverts commit 27b812229a9a59aeeede7d8b87498148d4baf4fa.

---
+++ src/lib/ngx-loading-control.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxLoadingControlComponent } from './ngx-loading-control.component';
+
+
+
+@NgModule({
+  declarations: [NgxLoadingControlComponent],
+  imports: [
+  ],
+  exports: [NgxLoadingControlComponent]
+})
+export class NgxLoadingControlModule { }
